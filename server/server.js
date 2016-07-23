08b825c346a54ee1606fc9ba399bd3aa49ff8bd3
M0ss Man <ghostreader45@hotmail.com>
2016-07-23 09:34:18 -0700

    basic server/client template setup

--- server/server.js
+++ server/server.js
@@ -3,6 +3,15 @@ var app = express();
 
 var portSet = 8080;
 
+app.set('port', portSet)
+
+app.use(function(req, res, next){
+  console.log('received a ' + req.method + ' request on url ' + req.path);
+  next();
+})
+
+app.get('/', express.static('./client'));
+
 app.listen(app.get('port'), function(){
   console.log('listening on port ' + portSet);
 })
